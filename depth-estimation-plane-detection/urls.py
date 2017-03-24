6df69e0a7f5e20640b9d5550a5a3b1f7690aea2d
Honey <512052217@qq.com>
2017-03-24 17:26:27 +0800

    m

--- depth-estimation-plane-detection/urls.py
+++ depth-estimation-plane-detection/urls.py
@@ -1,4 +1,4 @@
-"""test_django URL Configuration
+"""depth-estimation-and-plane-detection URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
